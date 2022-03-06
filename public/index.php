e99f1ce1bb62ff2e5f46fce21e80d31e7b7d5095
Vladimirs <vladimirribakov123@mail.ru>
2022-03-06 19:26:42 +0000

    populate main page with content

--- public/index.php
+++ public/index.php
@@ -1,20 +1,40 @@
 <?php require_once('../private/initialize.php'); ?>
 
+<?php  
+  if(isset($_GET['id'])) {
+    $page_id = $_GET['id'];
+    $page = find_page_by_id($page_id);
+    if(!$page) {
+      redirect_to(url_for('/index.php'));
+    }
+  } else {
+    // nothing selected how the homepage
+  }
+?>
+
 <?php include(SHARED_PATH . '/public_header.php'); ?>
 
 <div id="main">
 
-  <?php include(SHARED_PATH . '/public_navigation.php'); ?>
+    <?php include(SHARED_PATH . '/public_navigation.php'); ?>
 
-  <div id="page">
+    <div id="page">
 
-  <?php 
-  
-  include(SHARED_PATH . '/static_homepage.php'); 
+        <?php 
+
+  if(isset($page)) {
+    //show the page from database
+    // TODO add html escape 
+    echo $page['content'];
+  } else {
+
+    include(SHARED_PATH . '/static_homepage.php'); 
+  }
+
 
   ?>
 
-  </div>
+    </div>
 
 </div>
 
