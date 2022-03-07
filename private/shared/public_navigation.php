644f8868564dce7b0e13b41afd984bd7ccdccf71
Vladimirs <vladimirribakov123@mail.ru>
2022-03-07 14:34:59 +0000

    preview content func

--- private/shared/public_navigation.php
+++ private/shared/public_navigation.php
@@ -2,10 +2,11 @@
 
 $page_id = $page_id ?? '';
 $subject_id = $subject_id ?? '';
+$visible = $visible ?? true;
 ?>
 
 <navigation>
-    <?php $nav_subjects = find_all_subjects(['visible' => true]); ?>
+    <?php $nav_subjects = find_all_subjects(['visible' => $visible]); ?>
     <ul class="subjects">
         <?php while($nav_subject = mysqli_fetch_assoc($nav_subjects)) { ?>
         <?php //if(!$nav_subject['visible']) { continue; } ?>
@@ -14,7 +15,7 @@ $subject_id = $subject_id ?? '';
                 <?php echo h($nav_subject['menu_name']); ?>
             </a>
             <?php if($nav_subject['id'] == $subject_id) { ?>
-            <?php $nav_pages = find_pages_by_subject_id($nav_subject['id'], ['visible' => true]); ?>
+            <?php $nav_pages = find_pages_by_subject_id($nav_subject['id'], ['visible' => $visible]); ?>
             <ul class="pages">
                 <?php while($nav_page = mysqli_fetch_assoc($nav_pages)) { ?>
                 <?php //if(!$nav_page['visible']) { continue; } ?>
