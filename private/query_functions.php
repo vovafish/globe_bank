a00addac1b9b3e9288684f4700b9c07e28098c59
Vladimirs <vladimirribakov123@mail.ru>
2022-03-07 13:40:12 +0000

    protect page visibility

--- private/query_functions.php
+++ private/query_functions.php
@@ -17,11 +17,16 @@
     return $result;
   }
 
-  function find_subject_by_id($id) {
+  function find_subject_by_id($id, $options=[]) {
     global $db;
 
+    $visible = $options['visible'] ?? false;
+      
     $sql = "SELECT * FROM subjects ";
-    $sql .= "WHERE id='" . db_escape($db, $id) . "'";
+    $sql .= "WHERE id='" . db_escape($db, $id) . "' ";
+    if($visible) {
+      $sql .= "AND visible = true";
+    }
     // echo $sql;
     $result = mysqli_query($db, $sql);
     confirm_result_set($result);
@@ -146,11 +151,16 @@
     return $result;
   }
 
-  function find_page_by_id($id) {
+  function find_page_by_id($id, $options=[]) {
     global $db;
 
+    $visible = $options['visible'] ?? false;
+
     $sql = "SELECT * FROM pages ";
-    $sql .= "WHERE id='" . db_escape($db, $id) . "'";
+    $sql .= "WHERE id='" . db_escape($db, $id) . "' ";
+    if($visible) {
+      $sql .= "AND visible = true";
+    }
     $result = mysqli_query($db, $sql);
     confirm_result_set($result);
     $page = mysqli_fetch_assoc($result);
