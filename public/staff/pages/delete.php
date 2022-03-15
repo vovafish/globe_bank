0437f39a26c66a6f4a9fd90d343bda2345dc106e
Vladimirs <vladimirribakov123@mail.ru>
2022-03-15 21:43:12 +0000

    use nested redirectes

--- public/staff/pages/delete.php
+++ public/staff/pages/delete.php
@@ -8,14 +8,14 @@ if(!isset($_GET['id'])) {
 }
 $id = $_GET['id'];
 
+$page = find_page_by_id($id);
+
 if(is_post_request()) {
 
   $result = delete_page($id);
   $_SESSION['message'] = 'The page was deleted successfully.';
-  redirect_to(url_for('/staff/pages/index.php'));
+  redirect_to(url_for('/staff/subjects/show.php?id=' . h(u($page['subject_id']))));
 
-} else {
-  $page = find_page_by_id($id);
 }
 
 ?>
