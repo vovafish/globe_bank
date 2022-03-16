25753846e52346f90b0590956fb18d4806d28a71
Vladimirs <vladimirribakov123@mail.ru>
2022-03-16 19:19:40 +0000

    update positions

--- private/query_functions.php
+++ private/query_functions.php
@@ -74,6 +74,8 @@
       return $errors;
     }
 
+    shift_subject_positions(0, $subject['position']);
+
     $sql = "INSERT INTO subjects ";
     $sql .= "(menu_name, position, visible) ";
     $sql .= "VALUES (";
@@ -101,6 +103,10 @@
       return $errors;
     }
 
+    $old_subject = find_subject_by_id($subject['id']);
+    $old_position = $old_subject['position'];
+    shift_subject_positions($old_position, $subject['position'], $subject['id']);
+
     $sql = "UPDATE subjects SET ";
     $sql .= "menu_name='" . db_escape($db, $subject['menu_name']) . "', ";
     $sql .= "position='" . db_escape($db, $subject['position']) . "', ";
@@ -123,6 +129,10 @@
 
   function delete_subject($id) {
     global $db;
+
+    $old_subject = find_subject_by_id($id);
+    $old_position = $old_subject['position'];
+    shift_subject_positions($old_position, 0, $id);
 
     $sql = "DELETE FROM subjects ";
     $sql .= "WHERE id='" . db_escape($db, $id) . "' ";
@@ -222,6 +232,8 @@
       return $errors;
     }
 
+    shift_page_positions(0, $page['position'], $page['subject_id']);
+
     $sql = "INSERT INTO pages ";
     $sql .= "(subject_id, menu_name, position, visible, content) ";
     $sql .= "VALUES (";
@@ -251,6 +263,11 @@
       return $errors;
     }
 
+    $old_page = find_page_by_id($page['id']);
+    $old_position = $old_page['position'];
+    shift_page_positions($old_position ,$page['position'], $page['subject_id'], $page['id']);
+
+
     $sql = "UPDATE pages SET ";
     $sql .= "subject_id='" . db_escape($db, $page['subject_id']) . "', ";
     $sql .= "menu_name='" . db_escape($db, $page['menu_name']) . "', ";
@@ -275,6 +292,10 @@
 
   function delete_page($id) {
     global $db;
+
+    $old_page = find_page_by_id($id);
+    $old_position = $old_page['position'];
+    shift_page_positions($old_position, 0, $old_page['subject_id'], $id);
 
     $sql = "DELETE FROM pages ";
     $sql .= "WHERE id='" . db_escape($db, $id) . "' ";
@@ -502,6 +523,75 @@
       return true;
     } else {
       // DELETE failed
+      echo mysqli_error($db);
+      db_disconnect($db);
+      exit;
+    }
+  }
+
+  function shift_subject_positions($start_pos, $end_pos, $current_id=0) {
+    global $db;
+
+    if($start_pos == $end_pos){ return; }
+
+    $sql = "UPDATE subjects ";
+    if($start_pos == 0){
+      $sql .= "SET position = position + 1 ";
+      $sql .= "WHERE position  >= '" . db_escape($db, $end_pos) . "' ";
+    } elseif ($end_pos == 0) {
+      $sql .= "SET position = position - 1 ";
+      $sql .= "WHERE position  > '" . db_escape($db, $start_pos) . "' ";
+    } elseif($start_pos < $end_pos){
+      $sql .= "SET position = position - 1 ";
+      $sql .= "WHERE position  > '" . db_escape($db, $start_pos) . "' ";
+      $sql .= "AND position <= '" . db_escape($db, $end_pos) . "' ";
+    } elseif($start_pos > $end_pos){
+      $sql .= "SET position = position + 1 ";
+      $sql .= "WHERE position  >= '" . db_escape($db, $end_pos) . "' ";
+      $sql .= "AND position < '" . db_escape($db, $start_pos) . "' ";
+    }
+    $sql .= "AND id != '" . db_escape($db, $current_id) . "' ";
+
+    $result = mysqli_query($db, $sql);
+
+    if($result){
+      return true;
+    } else {
+      echo mysqli_error($db);
+      db_disconnect($db);
+      exit;
+    }
+  }
+
+  function shift_page_positions($start_pos, $end_pos, $subject_id, $current_id=0) {
+    global $db;
+
+    if($start_pos == $end_pos){ return; }
+
+    $sql = "UPDATE pages ";
+    if($start_pos == 0){
+      $sql .= "SET position = position + 1 ";
+      $sql .= "WHERE position  >= '" . db_escape($db, $end_pos) . "' ";
+    } elseif ($end_pos == 0) {
+      $sql .= "SET position = position - 1 ";
+      $sql .= "WHERE position  > '" . db_escape($db, $start_pos) . "' ";
+    } elseif($start_pos < $end_pos){
+      $sql .= "SET position = position - 1 ";
+      $sql .= "WHERE position  > '" . db_escape($db, $start_pos) . "' ";
+      $sql .= "AND position <= '" . db_escape($db, $end_pos) . "' ";
+    } elseif($start_pos > $end_pos){
+      $sql .= "SET position = position + 1 ";
+      $sql .= "WHERE position  >= '" . db_escape($db, $end_pos) . "' ";
+      $sql .= "AND position < '" . db_escape($db, $start_pos) . "' ";
+    }
+    $sql .= "AND id != '" . db_escape($db, $current_id) . "' ";
+    $sql .= "AND subject_id = '" . db_escape($db, $subject_id) . "'";
+
+    $result = mysqli_query($db, $sql);
+
+    if($result){
+      return true;
+    } else {
       echo mysqli_error($db);
       db_disconnect($db);
       exit;
